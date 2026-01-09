36f68f15ea9162d2ba2fcbc6176fdf627be7cf66
MrBitasmblTester2 <lulu2214@proton.me>
2026-01-09 13:29:35 +0400

    Add ImageToPng.vue

---
+++ frontend/src/components/ImageToPng.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="file" @change="onFile" accept="image/*" />
+    <button @click="convert">Convert</button>
+    <a v-if="url" :href="url">Download PNG</a>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const file = ref();
+const url = ref("");
+const onFile = e => (file.value = e.target.files[0]);
+const convert = async () => {
+  url.value = "";
+};
+</script>
