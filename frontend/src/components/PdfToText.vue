df0c9e60f7d7bd72583814ce251250e026d90f3b
MrBitasmblTester2 <lulu2214@proton.me>
2026-01-09 13:29:34 +0400

    Add PdfToText.vue

---
+++ frontend/src/components/PdfToText.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="file" @change="onFile" accept="application/pdf" />
+    <button @click="convert">Convert</button>
+    <pre>{{ text }}</pre>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const file = ref();
+const text = ref("");
+const onFile = e => (file.value = e.target.files[0]);
+const convert = async () => {
+  text.value = "";
+};
+</script>
