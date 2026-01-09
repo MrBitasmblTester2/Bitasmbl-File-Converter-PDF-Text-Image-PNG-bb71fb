b894438198919a1c4f02914ebf40207a597ac987
MrBitasmblTester2 <lulu2214@proton.me>
2026-01-09 13:29:33 +0400

    Add ConverterView.vue

---
+++ frontend/src/views/ConverterView.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="space-y-6">
+    <PdfToText />
+    <ImageToPng />
+  </section>
+</template>
+<script setup>
+import PdfToText from "../components/PdfToText.vue";
+import ImageToPng from "../components/ImageToPng.vue";
+</script>
