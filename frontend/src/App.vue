79a505c8568740174c662d943de54332d90b767d
MrBitasmblTester2 <lulu2214@proton.me>
2026-01-09 13:29:30 +0400

    Add App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen bg-gray-100">
+    <header class="p-4 bg-white shadow">File Converter</header>
+    <main class="p-4"><ConverterView /></main>
+  </div>
+</template>
+<script setup>
+import ConverterView from "./views/ConverterView.vue";
+</script>
